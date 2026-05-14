01fb478d3a70ecbe646955fba2b7d02b975f4933
sakesaur-89 <roma.gogeshvili@mail.ru>
2026-05-14 20:02:28 +0300

    set up the framework

---
+++ ClayMod/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ClayMod
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "ClayMod";
+        public const string PLUGIN_NAME = "My first plugin";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
